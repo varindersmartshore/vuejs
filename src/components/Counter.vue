97a296e9606d578d4fcee679a3942ceb0d2a51db
Varinder Kaur <varinder@smartshore.nl>
2021-08-10 12:13:39 +0530

    Initial Commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,49 @@
+<template>
+
+<section class="section">
+  <div class="container">
+    <h2 class="title">{{title}}</h2>
+    <div class="level">
+      <div class="level-item">
+        <button class="button" @click="incrementCounter">
+        <span class="icon">
+        <i class="fa fa-plus"></i>
+        </span>
+        </button>
+      </div>
+      <div class="level-item">
+        <h2>{{counter}}</h2>
+      </div>
+      <div class="level-item">
+        <button class="button" @click="decrementCounter">
+        <span class="icon">
+        <i class="fa fa-minus"></i>
+        </span>
+        </button>
+      </div>
+    </div>
+  </div>
+</section>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Counter",
+      counter: 0
+    };
+  },
+  methods: {
+    incrementCounter() {
+      if(this.counter == 10) return
+      this.counter++;
+    },
+    decrementCounter() {
+      if(this.counter == 0) return
+      this.counter--;
+    }
+  }
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+      <img alt="Vue logo" src="../assets/logo.png">
+      <Counter/>
+    <h1>This is a home page</h1>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import Counter from '@/components/Counter.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Counter
+  }
+}
+</script>
